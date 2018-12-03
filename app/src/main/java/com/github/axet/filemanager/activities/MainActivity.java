165e54c58948a68c9519cd55c791e6cceb951f61
Alexey Kuznetsov <axet@me.com>
2018-12-04 01:19:13 +0300

    save bookmarks on delete

--- app/src/main/java/com/github/axet/filemanager/activities/MainActivity.java
+++ app/src/main/java/com/github/axet/filemanager/activities/MainActivity.java
@@ -735,6 +735,7 @@ public class MainActivity extends AppCompatThemeActivity implements NavigationVi
                         @Override
                         public void onClick(DialogInterface dialog, int which) {
                             app.bookmarks.remove(uri);
+                            app.bookmarks.save();
                             reloadMenu();
                         }
                     });
